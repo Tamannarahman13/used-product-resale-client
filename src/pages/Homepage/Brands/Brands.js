148dd30a442e2893247618bad07c09c1008acefc
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-12-02 08:31:10 +0600

    category

--- src/pages/Homepage/Brands/Brands.js
+++ src/pages/Homepage/Brands/Brands.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import brand1 from "../../../assets/image/brands/brand-1.png";
-import brand2 from "../../../assets/image/brands/brand-2.png";
-import brand3 from "../../../assets/image/brands/brand-3.png";
-import brand4 from "../../../assets/image/brands/brand-4.png";
+import brand1 from "../../../assets/image/huawei brand.jpg";
+import brand2 from "../../../assets/image/realme brand.jpg";
+import brand3 from "../../../assets/image/samsung brand.png";
+import brand4 from "../../../assets/image/oppo brand.png";
 import "swiper/css/bundle";
 const Brands = () => {
   const brandsPhoto = [
@@ -20,11 +20,9 @@ const Brands = () => {
   return (
     <div div className="container mx-auto my-5">
       <div className="my-10">
-        <h2 className="text-center text-4xl font-bold my-5">Our Brands</h2>
+        <h2 className="text-center text-4xl font-bold my-5">Category</h2>
         <p className="w-2/3 mx-auto  mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-          voluptatum laboriosam eum veritatis magnam eius exercitationem
-          eligendi consequuntur nemo quidem!
+          A mobile phone is a wireless handheld device that allows users to make and receive calls. While the earliest generation of mobile phones could only make and receive calls, today's mobile phones do a lot more, accommodating web browsers, games, cameras, video players and navigational systems
         </p>
       </div>
       <Swiper
